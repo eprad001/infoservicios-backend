0bea04526b9b8e6a7308aca0449f0868cc1bb9b7
eprad001 <elizabethprado@gmail.com>
2025-08-27 17:03:28 -0400

    ..

--- routes/categoriasRoutes.js
+++ routes/categoriasRoutes.js
@@ -4,8 +4,8 @@ import controller from '../src/controllers/categoriasController.js';
 import auth from '../middleware/auth.js';
 import { authorize, ROLES } from '../middleware/roles.js';
 
-router.get('/', auth, authorize(ROLES.ADMIN, ROLES.CLIENTE, ROLES.TRABAJADOR), controller.getAll);
-router.get('/:id', auth, authorize(ROLES.ADMIN, ROLES.CLIENTE, ROLES.TRABAJADOR), controller.getById);
+router.get('/', auth, controller.getAll);
+router.get('/:id', auth, controller.getById);
 router.post('/', auth, authorize(ROLES.ADMIN), controller.create);
 router.put('/:id', auth, authorize(ROLES.ADMIN), controller.update);
 router.delete('/:id', auth, authorize(ROLES.ADMIN), controller.remove);
